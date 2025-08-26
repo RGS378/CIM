874ce3575e9d37d4b8193fece8f99aa634cd2768
S. Silva Domingos <suyannegomes01@gmail.com>
2025-08-26 12:31:46 -0300

    Update saeb.py

--- saeb.py
+++ saeb.py
@@ -1,73 +1,77 @@
-MediaM = float(input('Nota de matemática: '))
-MediaP = float(input('Nota de Português: '))
+from decimal import Decimal, getcontext
 
-DPm = 59
-DPp = 56
+# Define a precisão global 
+getcontext().prec = 10
 
-SinfM = MediaM - 3 * (DPm)
-SinfP = MediaP - 3 * (DPp)
+# Entradas com Decimal
+MediaM = Decimal(input('Nota de matemática: '))
+MediaP = Decimal(input('Nota de Português: '))
 
-SsupM = MediaM + 3 * (DPm)
-SsupP = MediaP + 3 * (DPp)
+DPm = Decimal('59')
+DPp = Decimal('56')
+
+SinfM = MediaM - Decimal('3') * DPm
+SinfP = MediaP - Decimal('3') * DPp
+
+SsupM = MediaM + Decimal('3') * DPm
+SsupP = MediaP + Decimal('3') * DPp
 
 print('==== SOBRE SUAS NOTAS ====')
-print('Sua menor nota em Matemática é: {} '.format(SinfM))
-print('Sua menor nota em Português é: {} '.format(SinfP))
-print('Sua maior nota em Matemática é: {} '.format(SsupM))
-print('Sua maior nota em Português é: {} '.format(SsupP))
+print(f'Sua menor nota em Matemática é: {SinfM}')
+print(f'Sua menor nota em Português é: {SinfP}')
+print(f'Sua maior nota em Matemática é: {SsupM}')
+print(f'Sua maior nota em Português é: {SsupP}')
 
 MediaExM = SsupM - SinfM
 MediaExP = SsupP - SinfP
 
 print('==== SOBRE SUAS MÉDIAS ====')
-print('A media extrema de Matematica é {}'.format(MediaExM))
-print('A media extrema de Português é {}'.format(MediaExP))
+print(f'A média extrema de Matemática é {MediaExM}')
+print(f'A média extrema de Português é {MediaExP}')
 
 print('==== SOBRE A ESCOLA ====')
-EscolaM = float(input('Nota média da escola em matématica é: '))
-EscolaP = float(input('Nota média da escola em português é: '))
+EscolaM = Decimal(input('Nota média da escola em matemática é: '))
+EscolaP = Decimal(input('Nota média da escola em português é: '))
 
-ProficienciaP = ((EscolaP - SinfP) / MediaExP) * 10
-ProficienciaM = ((EscolaM - SinfM) / MediaExM) * 10
+ProficienciaP = ((EscolaP - SinfP) / MediaExP) * Decimal('10')
+ProficienciaM = ((EscolaM - SinfM) / MediaExM) * Decimal('10')
 
-print('A proficiência de Matematica é {} e a de Português é {} '.format(ProficienciaM, ProficienciaP))
+print(f'A proficiência de Matemática é {ProficienciaM} e a de Português é {ProficienciaP}')
 
-MediaTotal = (ProficienciaM + ProficienciaP) / 2
-
-print('A media total é {}'.format(MediaTotal))
+MediaTotal = (ProficienciaM + ProficienciaP) / Decimal('2')
+print(f'A média total é {MediaTotal}')
 
 print('==== SOBRE A APROVAÇÃO ====')
-Alunos1 = float(input('Quantos alunos tem na 1 série? '))
-Alunos2 = float(input('Quantos alunos tem na 2 série? '))
-Alunos3 = float(input('Quantos alunos tem na 3 série? '))
+Alunos1 = Decimal(input('Quantos alunos tem na 1ª série? '))
+Alunos2 = Decimal(input('Quantos alunos tem na 2ª série? '))
+Alunos3 = Decimal(input('Quantos alunos tem na 3ª série? '))
 
-Aprovados1 = float(input('Quantos alunos aprovados na 1ª série? '))
-Aprovados2 = float(input('Quantos alunos aprovados na 2ª série? '))
-Aprovados3 = float(input('Quantos alunos aprovados na 3ª série? '))
+Aprovados1 = Decimal(input('Quantos alunos aprovados na 1ª série? '))
+Aprovados2 = Decimal(input('Quantos alunos aprovados na 2ª série? '))
+Aprovados3 = Decimal(input('Quantos alunos aprovados na 3ª série? '))
 
 TaxaDeAprovacao1 = Aprovados1 / Alunos1
 TaxaDeAprovacao2 = Aprovados2 / Alunos2
 TaxaDeAprovacao3 = Aprovados3 / Alunos3
 
+print(f'Taxa de aprovação da 1ª série: {TaxaDeAprovacao1}')
+print(f'Taxa de aprovação da 2ª série: {TaxaDeAprovacao2}')
+print(f'Taxa de aprovação da 3ª série: {TaxaDeAprovacao3}')
 
-print('Taxa de aprovação da 1ª série:', TaxaDeAprovacao1)
-print('Taxa de aprovação da 2ª série:', TaxaDeAprovacao2)
-print('Taxa de aprovação da 3ª série:', TaxaDeAprovacao3)
+Aprovacao1 = Decimal('1') / TaxaDeAprovacao1
+Aprovacao2 = Decimal('1') / TaxaDeAprovacao2
+Aprovacao3 = Decimal('1') / TaxaDeAprovacao3
 
-Aprovacao1 = 1/TaxaDeAprovacao1
-Aprovacao2 = 1/TaxaDeAprovacao2
-Aprovacao3 = 1/TaxaDeAprovacao3
-
-print('Aprovaçaõ do 1 série é {}, 2 série é {}, 3 série é {}. '.format(Aprovacao1, Aprovacao2, Aprovacao3))
+print(f'Aprovação da 1ª série é {Aprovacao1}, 2ª série é {Aprovacao2}, 3ª série é {Aprovacao3}')
 Total = Aprovacao1 + Aprovacao2 + Aprovacao3
 
-print('O total de aprovado {}'.format(Total))
+print(f'O total de aprovado é {Total}')
 
-Tempo = Total/3
-print('O tempo de aprovado {}'.format(Tempo))
+Tempo = Total / Decimal('3')
+print(f'O tempo de aprovado é {Tempo}')
 
-Padronizado = 1/Tempo
-print('O padronizado {}'.format(Padronizado))
+Padronizado = Decimal('1') / Tempo
+print(f'O padronizado é {Padronizado}')
 
 IDEB = MediaTotal * Padronizado
-print('A nota da IDEB é {}'.format(IDEB))
+print(f'A nota da IDEB é {IDEB}')
